096c7dd0a297f3049595daaab07d81fe3dced9ab
Inchara-Angadi <inchara.angadi214@gmail.com>
2025-05-17 22:16:34 +0530

    Add files via upload

---
+++ ColorSearch.java
@@ -0,0 +1,28 @@
+package program;
+import java.util.*;
+public class ColorSearch {
+	
+	    public static void main(String[] args) {
+	        // Create an ArrayList of colors
+	        ArrayList<String> colors = new ArrayList<>();
+
+	        // Add colors to the list
+	        colors.add("Blue");
+	        colors.add("Green");
+	        colors.add("Yellow");
+	        colors.add("Red");
+	        colors.add("White");
+
+	        // Display the list
+	        System.out.println("Available colors: " + colors);
+
+	        // Check if "Red" is in the list
+	        if (colors.contains("Red")) {
+	            System.out.println("The color 'Red' is available in the list.");
+	        } else {
+	            System.out.println("The color 'Red' is NOT available in the list.");
+	        }
+	    }
+	}
+
+
